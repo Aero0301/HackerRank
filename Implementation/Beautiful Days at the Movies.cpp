6b24e0bba59e980f9f2de5b09c66b882620aa9e5
Aero0301 <aviralomar0301@gmail.com>
2019-12-25 15:26:55 +0530

    Update Beautiful Days at the Movies.cpp

--- Implementation/Beautiful Days at the Movies.cpp
+++ Implementation/Beautiful Days at the Movies.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int reverseDifference(int num) {
+	int rev = 0, temp = num, diff;
+	while (temp != 0) {
+		rev = rev * 10 + temp % 10;
+		temp /= 10;
+	}
+	diff = rev - num;
+	return (diff > 0 ? diff : diff * -1);
+}
+
+int main() {
+	int i, j, k, count;
+	cin >> i >> j >> k;
+	for (int m = i; m <= j; m++) {
+		if (reverseDifference(m) % k == 0) {
+			++count;
+		}
+	}
+	cout << count << flush;
+
+	return 0;
+}
